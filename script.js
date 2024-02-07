a81acd227737d987538aeb510b1fc8d0da5904cd
Rohangubbala <159234474+Rohangubbala@users.noreply.github.com>
2024-02-07 12:56:29 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("titleLink").addEventListener("click", function() {
-    var audio = document.getElementById("voiceMessage");
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-        audio.currentTime = 0;
-    }
-});
